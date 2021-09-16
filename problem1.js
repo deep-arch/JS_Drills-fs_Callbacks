8d6a31bda6832db6d3d4c277ba7404b50849e8af
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-09-16 14:53:18 +0530

    First problem

---
+++ problem1.js
@@ -0,0 +1,83 @@
+// Problem 1:
+
+// Using callbacks and the fs module's asynchronous functions, do the following:
+//     1. Create a directory of random JSON files
+//     2. Delete those files simultaneously
+
+const fs = require("fs");
+
+function problem1(filename, maxNumberFiles, callback) {
+  createDir((error, data) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, data);
+      for (let index = 1; index < Math.random() * maxNumberFiles + 2; index++) {
+        console.log(index);
+        createFile(filename + index, (error, data) => {
+          if (error) {
+            callback(error);
+          } else {
+            callback(null, data);
+            deleteFile(filename + index, (error, data) => {
+              if (error) {
+                callback(error);
+              } else {
+                callback(null, data);
+                deleteDir((error, data) => {
+                  if (error) {
+                    callback(error);
+                  } else {
+                    callback(null, data);
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    }
+  });
+}
+
+function createDir(callback) {
+  fs.mkdir("directory", (error) => {
+    if (error) {
+      callback(new Error("Directory already exists!"));
+    } else {
+      callback(null, "Directory created successfully!");
+    }
+  });
+}
+
+function createFile(filename, callback) {
+  fs.writeFile(`./directory/${filename}.json`, "JSONdata", (error) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, `${filename}.json file created successfully!`);
+    }
+  });
+}
+
+function deleteFile(filename, callback) {
+  fs.unlink(`./directory/${filename}.json`, (error) => {
+    if (error) {
+      callback(new Error("File does not exists!"));
+    } else {
+      callback(null, `${filename}.json file deleted successfully!`);
+    }
+  });
+}
+
+function deleteDir(callback) {
+  fs.rmdir("directory", function (error) {
+    if (error) {
+      return;
+    } else {
+      callback(null, "");
+    }
+  });
+}
+
+module.exports = { problem1 };
---
+++ test/testProblem1.js
@@ -0,0 +1,20 @@
+const { problem1 } = require("../problem1");
+
+function fileNameGen() {
+  var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var result = "";
+  for (let index = 1; index < 5; index++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+let maxNumberFiles = 10;
+
+problem1(fileNameGen(), maxNumberFiles, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
